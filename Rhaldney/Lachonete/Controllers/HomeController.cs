dbe510b748bddf6b132484f5b1977e5df3d53973
Julio Mendes <juliomendes160@hotmail.com>
2021-12-24 13:35:16 -0300

    Update

--- Rhaldney/Lachonete/Controllers/HomeController.cs
+++ Rhaldney/Lachonete/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Lachonete.Controllers
             return View();
         }
 
+        public IActionResult Compras()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
