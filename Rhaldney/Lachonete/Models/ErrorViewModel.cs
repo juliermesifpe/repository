f2ccca1429652602f2b2bc45d705f6d8113478ce
Julio Mendes <juliomendes160@hotmail.com>
2021-12-24 08:51:42 -0300

    Initial commit

---
+++ Rhaldney/Lachonete/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lachonete.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
